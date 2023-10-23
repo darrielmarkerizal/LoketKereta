27d173f9326117c07218b3151bd949aee8b434cb
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-10-23 16:32:58 +0700

    feat(intent) : logic login to dashboard

--- app/src/main/java/com/example/loketkereta/LoginActivity.kt
+++ app/src/main/java/com/example/loketkereta/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.loketkereta
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
@@ -24,7 +25,8 @@ class LoginActivity : AppCompatActivity() {
             val password = passwordInput.text.toString()
 
             if (validateLogin(username, password)) {
-                Toast.makeText(this, "Login berhasil", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, DashboardActivity::class.java)
+                startActivity(intent)
             } else {
                 Toast.makeText(this, "Login gagal. Pastikan data yang Anda masukkan benar.", Toast.LENGTH_SHORT).show()
             }
