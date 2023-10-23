f74aa7f1c1e630dd7465fd12cf82a39be892fe40
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-10-24 06:41:19 +0700

    feat(booking) : add chip and total harga, string array stasiun dan kelas

--- app/src/main/java/com/example/loketkereta/DashboardActivity.kt
+++ app/src/main/java/com/example/loketkereta/DashboardActivity.kt
@@ -2,10 +2,20 @@ package com.example.loketkereta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.view.View
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val pesanTiketCardView = findViewById<View>(R.id.pesan_tiket)
+
+        pesanTiketCardView.setOnClickListener {
+            val intent = Intent(this, BookingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
