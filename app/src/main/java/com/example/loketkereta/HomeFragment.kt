0464131a997e1c76da2246c912222dee3939c4c2
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-18 10:14:35 +0700

    datepicker

--- app/src/main/java/com/example/loketkereta/HomeFragment.kt
+++ app/src/main/java/com/example/loketkereta/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.loketkereta
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -15,7 +16,10 @@ import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 import android.util.Log
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.example.loketkereta.stasiun.Stasiun
 import com.example.loketkereta.stasiun.StationApi
 import com.google.firebase.firestore.DocumentSnapshot
@@ -24,6 +28,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -46,32 +53,13 @@ class HomeFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
-        fetchUserName()
-
-        val call = api.getStations()
-        call.enqueue(object : Callback<List<Stasiun>> {
-            override fun onResponse(call: Call<List<Stasiun>>, response: Response<List<Stasiun>>) {
-                if (response.isSuccessful) {
-                    val stations = response.body()
-                    val stationDetails = stations?.map { "${it.name}, ${it.cityName}" } ?: listOf()
-
-                    val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, stationDetails)
-                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
-                    binding.spinnerKeberangkatan.adapter = adapter
-                    binding.spinnerTujuan.adapter = adapter
-                }
-            }
-
-            override fun onFailure(call: Call<List<Stasiun>>, t: Throwable) {
-                // Handle the error
-            }
-        })
+        fetchAndDisplayUserName()
+        setupStationsSpinner()
 
         return view
     }
 
-    private fun fetchUserName() {
+    private fun fetchAndDisplayUserName() {
         val userId = auth.currentUser?.uid
         if (userId != null) {
             val database = FirebaseDatabase.getInstance()
@@ -90,8 +78,101 @@ class HomeFragment : Fragment() {
         }
     }
 
+    private fun setupStationsSpinner() {
+        val call = api.getStations()
+        call.enqueue(object : Callback<List<Stasiun>> {
+            override fun onResponse(call: Call<List<Stasiun>>, response: Response<List<Stasiun>>) {
+                if (response.isSuccessful) {
+                    val stations = response.body()
+                    val stationDetails = stations?.map { Triple(it.code, "${it.name}, ${it.cityName}", it.code) }?.toMutableList() ?: mutableListOf()
+                    stationDetails.add(0, Triple("", "Pilih Stasiun", ""))
+
+                    val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, stationDetails.map { it.second })
+                    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+                    binding.spinnerKeberangkatan.adapter = adapter
+                    binding.spinnerTujuan.adapter = adapter
+
+                    binding.stasiunKeberangkatan.visibility = View.GONE
+                    binding.stasiunTujuan.visibility = View.GONE
+
+                    binding.spinnerKeberangkatan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                            if (position != 0) {
+                                val selectedStation = stationDetails[position]
+                                val stationName = selectedStation.second.split(",")[0].toLowerCase().capitalize()
+                                binding.stasiunKeberangkatan.setText("Stasiun " + stationName)
+                                binding.stasiunKeberangkatan.visibility = View.VISIBLE
+                            }
+                        }
+
+                        override fun onNothingSelected(parent: AdapterView<*>) {
+                            binding.stasiunKeberangkatan.visibility = View.GONE
+                        }
+                    }
+
+                    binding.spinnerTujuan.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                        override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                            if (position != 0) {
+                                val selectedStation = stationDetails[position]
+                                val stationName = selectedStation.second.split(",")[0].toLowerCase().capitalize()
+                                binding.stasiunTujuan.setText("Stasiun " + stationName)
+                                binding.stasiunTujuan.visibility = View.VISIBLE
+                            }
+                        }
+
+                        override fun onNothingSelected(parent: AdapterView<*>) {
+                            binding.stasiunTujuan.visibility = View.GONE
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<Stasiun>>, t: Throwable) {
+                // Handle the error
+            }
+        })
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupTanggalKeberangkatan()
+    }
+
+    private fun setupTanggalKeberangkatan() {
+        val tanggalKeberangkatanLayout = binding.layoutTanggal
+        val tanggalKeberangkatanTextView = binding.tanggalKeberangkatan
+
+        tanggalKeberangkatanLayout.setOnClickListener {
+            showDatePicker(tanggalKeberangkatanTextView)
+        }
+    }
+
+    private fun showDatePicker(textView: TextView) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(requireContext(), { _, selectedYear, selectedMonth, selectedDay ->
+            val formattedDate = formatSelectedDate(selectedYear, selectedMonth, selectedDay)
+            textView.text = formattedDate
+        }, year, month, day)
+
+        datePickerDialog.datePicker.minDate = System.currentTimeMillis()
+        datePickerDialog.show()
+    }
+
+    private fun formatSelectedDate(year: Int, month: Int, day: Int): String {
+        val selectedDate = Calendar.getInstance()
+        selectedDate.set(Calendar.YEAR, year)
+        selectedDate.set(Calendar.MONTH, month)
+        selectedDate.set(Calendar.DAY_OF_MONTH, day)
+        val date = selectedDate.time
+
+        val sdf = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale("id", "ID"))
+        return sdf.format(date)
     }
 
     override fun onDestroyView() {
