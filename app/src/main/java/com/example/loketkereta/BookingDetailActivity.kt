ba481acdf7b0449204b8797eb830ae1ea32f750c
Darriel Markerizal <darrielmarkerizal@gmail.com>
2023-12-21 12:02:02 +0700

    booking working

--- app/src/main/java/com/example/loketkereta/BookingDetailActivity.kt
+++ app/src/main/java/com/example/loketkereta/BookingDetailActivity.kt
@@ -3,12 +3,16 @@ package com.example.loketkereta
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import com.example.loketkereta.databinding.ActivityBookingDetailBinding
 import com.example.loketkereta.databinding.PassengerCardBinding
 import com.example.loketkereta.kereta.dataKereta
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class BookingDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBookingDetailBinding
@@ -17,6 +21,12 @@ class BookingDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityBookingDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val spinner = binding.passengerCard1.spinnerIdentitas
+        val adapter = ArrayAdapter.createFromResource(this, R.array.identitas, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.setSelection(0)
 
         val kereta = intent.getSerializableExtra("kereta") as dataKereta
 
@@ -52,6 +62,40 @@ class BookingDetailActivity : AppCompatActivity() {
                 binding.passengerCard1.linearLayout5.visibility = View.VISIBLE
                 binding.passengerCard1.linearLayout6.visibility = View.VISIBLE
                 binding.passengerCard1.textView3.visibility = View.VISIBLE
+            }
+        }
+
+        binding.btnLanjut.setOnClickListener {
+            val db = FirebaseFirestore.getInstance()
+            val auth = FirebaseAuth.getInstance()
+            val userId = auth.currentUser?.uid
+
+            if (userId != null) {
+                val namaPenumpang = binding.passengerCard1.editTextNamaPenumpang.text.toString()
+                val nomorIdentitas = binding.passengerCard1.editTextNomorIdentitas.text.toString()
+                val jenisIdentitas = binding.passengerCard1.spinnerIdentitas.selectedItem.toString()
+
+                val ticketData = hashMapOf(
+                    "userId" to userId,
+                    "kereta" to kereta,
+                    "namaPenumpang" to namaPenumpang,
+                    "nomorIdentitas" to nomorIdentitas,
+                    "jenisIdentitas" to jenisIdentitas
+                )
+
+                db.collection("tickets").document(userId)
+                    .set(ticketData)
+                    .addOnSuccessListener {
+                        Log.d("Firestore", "DocumentSnapshot successfully written!")
+                        val intent = Intent(this, BookingSuccessActivity::class.java)
+                        intent.putExtra("kereta", kereta)
+                        startActivity(intent)
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w("Firestore", "Error writing document", e)
+                    }
+            } else {
+                Log.w("Firestore", "User is not logged in")
             }
         }
     }
---
+++ app/src/main/java/com/example/loketkereta/BookingSuccessActivity.kt
@@ -0,0 +1,41 @@
+package com.example.loketkereta
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.loketkereta.databinding.ActivityBookingSuccessBinding
+import com.example.loketkereta.kereta.dataKereta
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class BookingSuccessActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBookingSuccessBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBookingSuccessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val kereta = intent.getSerializableExtra("kereta") as dataKereta
+
+        binding.jamKeberangkatan.text = kereta.jamBerangkat
+        binding.jamTiba.text = kereta.jamTiba
+        binding.kotaKeberangkatan.text = kereta.stasiunKeberangkatan
+        binding.kotaTiba.text = kereta.stasiunTujuan
+
+        val originalFormat = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+        val targetFormat = SimpleDateFormat("EEEE, dd MMM yy", Locale("in"))
+        val date = originalFormat.parse(kereta.tanggalBerangkat)
+        val formattedDate = targetFormat.format(date)
+        binding.tanggalKeberangkatan.text = formattedDate
+        binding.tanggalKeberangkatan.text = kereta.tanggalBerangkat
+        binding.waktuKeberangkatan.text = kereta.jamBerangkat
+
+        val buttonSelesai = binding.btnSelesai
+        buttonSelesai.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
